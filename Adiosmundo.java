cf9d9cc984bcb1d145cf7722ef66c436833dd9a1
Miguel <miguelarcediano6@gmail.com>
2019-10-03 18:27:53 +0200

    colores

--- Adiosmundo.java
+++ Adiosmundo.java
@@ -4,6 +4,7 @@ public class Adiosmundo {
 	public static void main(String[] args) {
 	String amarillo = "\033[33m";
 	String verde= "\033[32m";
+	String reset= "\033[32m"
 		System.out.println (amarillo + "Hola Patata");//Imprime el mensaje
                 System.out.println(verde + "Pero...volvere");
 
